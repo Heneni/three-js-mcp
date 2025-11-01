795c921677bd41a4a745738f2d7590e674a81eea
Heneni <mark@heneniart.com>
2025-11-01 15:22:22 -0400

    Simplify content paths in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      sans: ['Inter', 'Arial', 'sans-serif'],
-    },
     extend: {},
   },
   plugins: [],
-}
+};
