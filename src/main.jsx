59dfe305092ac2f01bf0a15fb508c09108a306a2
Heneni <mark@heneniart.com>
2025-11-01 12:50:05 -0400

    Initialize main.jsx with React application setup

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './showcase.jsx'
+import './styles.css'
+createRoot(document.getElementById('root')).render(<App />)
