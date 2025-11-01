ec40e15d58588766750599ae6247d9c6334b7874
Heneni <mark@heneniart.com>
2025-11-01 12:17:16 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './showcase.jsx'
-import './styles.css'
-
-createRoot(document.getElementById('root')).render(<App />)
